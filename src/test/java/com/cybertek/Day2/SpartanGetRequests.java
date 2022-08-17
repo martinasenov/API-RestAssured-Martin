83b8b64d87604f9750b8a530c2e50e95027a5f56
martinasenov <martin.manuilov.asenov@gmail.com>
2022-08-17 23:26:57 +0200

    Day2

---
+++ src/test/java/com/cybertek/Day2/SpartanGetRequests.java
@@ -0,0 +1,20 @@
+package com.cybertek.Day2;
+
+public class SpartanGetRequests {
+
+  String url="54.173.117.255:8000/api/spartans";
+
+
+  /*
+
+  Given Accept type application/json
+  When user send GET request to api/spartans end point
+  Then response Content Type must be application/json
+  And response body should include spartan result
+
+
+   */
+
+
+
+}
