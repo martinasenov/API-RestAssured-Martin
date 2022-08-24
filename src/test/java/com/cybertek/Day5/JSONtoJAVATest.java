613ceb1d0f4e53ea90718689a385dfe764135a4d
martinasenov <martin.manuilov.asenov@gmail.com>
2022-08-24 23:40:04 +0200

    Day5-DeSerialization test

---
+++ src/test/java/com/cybertek/Day5/JSONtoJAVATest.java
@@ -0,0 +1,41 @@
+package com.cybertek.Day5;
+
+import com.cybertek.Utilites.SpartanTestBase;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static io.restassured.RestAssured.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static io.restassured.RestAssured.*;
+
+
+public class JSONtoJAVATest extends SpartanTestBase {
+
+
+    @DisplayName("GET one Spartan and deserialize to Map")
+    @Test
+    public void oneSpartanToMap(){
+
+        Response response = given().pathParam("id", 15)
+                .when().get("/api/spartans/{id}")
+                .then().statusCode(200).extract().response();
+
+        //get the json and convert it to the map
+
+        Map<String,Object>jsonMap=response.as(Map.class);
+
+        System.out.println(jsonMap.toString());
+
+        //after we got the map, we can use hamcrest or Junit assertions to do assertion
+        String actualName = (String) jsonMap.get("name");
+        assertThat(actualName,is("Meta"));
+
+
+    }
+}
